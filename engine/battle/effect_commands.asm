b86ff90cd743ad713ca0f3e44a3a1303b15bdb87
8bitZeta <devin94@protonmail.com>
2023-03-17 00:19:35 -0500

    Multi-Player Battle Engine Bug Fix Thick Club and Light Ball can make Stat wrap around above 1024

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -2848,9 +2848,19 @@ DoubleStatIfSpeciesHoldingItem:
 	ret nz
 
 ; Double the stat
-; BUG: Thick Club and Light Ball can make (Special) Attack wrap around above 1024 (see docs/bugs_and_glitches.md)
 	sla l
 	rl h
+
+	ld a, HIGH(MAX_STAT_VALUE)
+	cp h
+	jr c, .cap
+	ret nz
+	ld a, LOW(MAX_STAT_VALUE)
+	cp l
+	ret nc
+
+.cap
+	ld hl, MAX_STAT_VALUE
 	ret
 
 EnemyAttackDamage:
