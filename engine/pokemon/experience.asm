f5b56683ded96da60f81a75c8438b1cb80c47449
8bitZeta <devin94@protonmail.com>
2023-03-18 02:12:30 -0500

    Single-Player Battle Engine Bug Experience underflow for level 1 Pokémon with Medium-Slow growth rate

--- engine/pokemon/experience.asm
+++ engine/pokemon/experience.asm
@@ -32,8 +32,18 @@ CalcLevel:
 
 CalcExpAtLevel:
 ; (a/b)*n**3 + c*n**2 + d*n - e
-; BUG: Experience underflow for level 1 Pokémon with Medium-Slow growth rate (see docs/bugs_and_glitches.md)
-	ld a, [wBaseGrowthRate]
+	ld a, d
+	dec a
+	jr nz, .UseExpFormula
+; Pokémon have 0 experience at level 1
+	ld hl, hProduct
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ld [hl], a
+	ret
+
+.UseExpFormula	ld a, [wBaseGrowthRate]
 	cp GROWTH_ERRATIC
 	jp z, .erratic
 	cp GROWTH_FLUCTUATING
