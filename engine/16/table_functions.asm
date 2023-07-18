GlitchR9506/pokecrystal16

Add move conversion table functions

engine/16/table_functions.asm
@@ -44,3 +44,59 @@ PokemonTableGarbageCollection:
 
 _LockPokemonID::
 	___conversion_table_lock_ID wPokemonIndexTable, MON_TABLE
+
+_GetMoveIndexFromID::
+	___conversion_table_load wMoveIndexTable, MOVE_TABLE
+
+_GetMoveIDFromIndex::
+	___conversion_table_store wMoveIndexTable, MOVE_TABLE
+	; fallthrough
+MoveTableGarbageCollection:
+	; must preserve de and rSVBK
+	push de
+	ldh a, [rSVBK]
+	push af
+	ld a, 1
+	ldh [rSVBK], a
+___move = 0
+	rept NUM_MOVES
+		___conversion_bitmap_check_structs wPartyMon1Moves + ___move, PARTYMON_STRUCT_LENGTH, PARTY_LENGTH, .set_bit
+		___conversion_bitmap_check_structs wBreedMon1Moves + ___move, wBreedMon2 - wBreedMon1, 2, .set_bit
+		ld a, [wEggMonMoves + ___move]
+		call .set_bit
+		; may or may not be valid
+		___conversion_bitmap_check_structs wOTPartyMon1Moves + ___move, PARTYMON_STRUCT_LENGTH, PARTY_LENGTH, .set_bit
+		___conversion_bitmap_check_values .set_bit, wBufferMonMoves + ___move, wTempMonMoves + ___move, wContestMonMoves + ___move, \
+		                                            wBattleMonMoves + ___move, wEnemyMonMoves + ___move, wWildMonMoves + ___move, \
+		                                            wPlayerUsedMoves + ___move
+___move = ___move + 1
+	endr
+	ld a, [wNamedObjectIndexBuffer] ;or any of its aliases...
+	call .set_bit
+	; only valid sometimes
+	___conversion_bitmap_check_values .set_bit, wCurPlayerMove, wCurEnemyMove, wDisabledMove, wEnemyDisabledMove, wUnusedPlayerLockedMove, \
+	                                            wLastPlayerMove, wLastEnemyMove, wLastPlayerCounterMove, wLastEnemyCounterMove, \
+	                                            wPlayerTrappingMove, wEnemyTrappingMove, wPlayerMoveStructAnimation, wEnemyMoveStructAnimation, \
+	                                            wPutativeTMHMMove
+	pop af
+	ldh [rSVBK], a
+	ldh a, [hSRAMBank]
+	push af
+	ld a, BANK(sBox)
+	call GetSRAMBank
+___move = 0
+	rept NUM_MOVES
+		___conversion_bitmap_check_structs sBoxMon1Moves + ___move, BOXMON_STRUCT_LENGTH, MONS_PER_BOX, .set_bit
+___move = ___move + 1
+	endr
+	pop af
+	call GetSRAMBank
+	___conversion_bitmap_free_unused wMoveIndexTable, MOVE_TABLE
+	pop de
+	ret
+
+.set_bit
+	___conversion_bitmap_set MOVE_TABLE
+
+_LockMoveID::
+	___conversion_table_lock_ID wMoveIndexTable, MOVE_TABLE

8bitZeta/modern-ancient-engine

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/16/table_functions.asm
@@ -44,3 +44,55 @@ PokemonTableGarbageCollection:
 
 _LockPokemonID::
 	___conversion_table_lock_ID wPokemonIndexTable, MON_TABLE
+
+_GetMoveIndexFromID::
+	___conversion_table_load wMoveIndexTable, MOVE_TABLE
+
+_GetMoveIDFromIndex::
+	___conversion_table_store wMoveIndexTable, MOVE_TABLE
+	; fallthrough
+MoveTableGarbageCollection:
+	; must preserve de and rSVBK
+	push de
+	ldh a, [rSVBK]
+	push af
+	ld a, 1
+	ldh [rSVBK], a
+	FOR ___move, NUM_MOVES
+		___conversion_bitmap_check_structs wPartyMon1Moves + ___move, PARTYMON_STRUCT_LENGTH, PARTY_LENGTH, .set_bit
+		___conversion_bitmap_check_structs wBreedMon1Moves + ___move, wBreedMon2 - wBreedMon1, 2, .set_bit
+		ld a, [wEggMonMoves + ___move]
+		call .set_bit
+		; may or may not be valid
+		___conversion_bitmap_check_structs wOTPartyMon1Moves + ___move, PARTYMON_STRUCT_LENGTH, PARTY_LENGTH, .set_bit
+		___conversion_bitmap_check_values .set_bit, wBufferMonMoves + ___move, wTempMonMoves + ___move, wContestMonMoves + ___move, \
+		                                            wBattleMonMoves + ___move, wEnemyMonMoves + ___move, wWildMonMoves + ___move, \
+		                                            wPlayerUsedMoves + ___move
+	endr
+	ld a, [wNamedObjectIndex] ;or any of its aliases...
+	call .set_bit
+	; only valid sometimes
+	___conversion_bitmap_check_values .set_bit, wCurPlayerMove, wCurEnemyMove, wDisabledMove, wEnemyDisabledMove, wUnusedPlayerLockedMove, \
+	                                            wLastPlayerMove, wLastEnemyMove, wLastPlayerCounterMove, wLastEnemyCounterMove, \
+	                                            wPlayerTrappingMove, wEnemyTrappingMove, wPlayerMoveStructAnimation, wEnemyMoveStructAnimation, \
+	                                            wPutativeTMHMMove
+	pop af
+	ldh [rSVBK], a
+	ldh a, [hSRAMBank]
+	push af
+	ld a, BANK(sBox)
+	call OpenSRAM
+	FOR ___move, NUM_MOVES
+		___conversion_bitmap_check_structs sBoxMon1Moves + ___move, BOXMON_STRUCT_LENGTH, MONS_PER_BOX, .set_bit
+	endr
+	pop af
+	call OpenSRAM
+	___conversion_bitmap_free_unused wMoveIndexTable, MOVE_TABLE
+	pop de
+	ret
+
+.set_bit
+	___conversion_bitmap_set MOVE_TABLE
+
+_LockMoveID::
+	___conversion_table_lock_ID wMoveIndexTable, MOVE_TABLE