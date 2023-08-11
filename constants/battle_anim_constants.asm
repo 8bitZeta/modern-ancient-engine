399f3309441e71d6178ae3c0ceec532a2f798bd9
8bitZeta <devin94@protonmail.com>
2023-08-11 01:13:38 -0500

    Updated Trick Animation
    Co-Authored-By: SoupPotato <22991349+SoupPotato@users.noreply.github.com>

--- constants/battle_anim_constants.asm
+++ constants/battle_anim_constants.asm
@@ -251,6 +251,7 @@ DEF BATTLEANIM_BASE_TILE EQU 7 * 7  ; Maximum size of a pokemon picture
 	const ANIM_OBJ_DROPLET_L
 	const ANIM_OBJ_LONG_PUNCH
 	const ANIM_OBJ_TAUNT
+	const ANIM_OBJ_TRICK
 DEF NUM_ANIM_OBJS EQU const_value
 
 ; DoBattleAnimFrame arguments (see engine/battle_anims/functions.asm)
--- data/moves/animations_2.asm
+++ data/moves/animations_2.asm
@@ -611,12 +611,24 @@ BattleAnim_Taunt:
 	anim_ret
 
 BattleAnim_Trick:
-	anim_1gfx ANIM_GFX_HIT
-	anim_sound 0, 1, SFX_POUND
-	anim_obj ANIM_OBJ_PALM, 136, 56, $0
-	anim_wait 6
-	anim_obj ANIM_OBJ_HIT_YFIX, 136, 56, $0
+	anim_1gfx ANIM_GFX_STATUS
+	anim_clearenemyhud
+	anim_bgeffect ANIM_BG_SHOW_MON, $0, $0, $0
+	anim_sound 0, 1, SFX_GET_COIN_FROM_SLOTS
+	anim_obj ANIM_OBJ_TRICK, 90, 68, $18
+	anim_obj ANIM_OBJ_TRICK, 90, 68, $38
 	anim_wait 16
+.loop
+	anim_sound 0, 1, SFX_STOP_SLOT
+	anim_wait 32
+	anim_loop 4, .loop
+	anim_wait 7
+	anim_sound 0, 1, SFX_SLOT_MACHINE_START
+	anim_incobj 1
+	anim_incobj 2
+	anim_wait 6
+	anim_clearobjs
+	anim_wait 6
 	anim_ret
 
 BattleAnim_RolePlay:
