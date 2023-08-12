eee625881a9d59bcd71623bd2853e6b9894e8b4b
8bitZeta <devin94@protonmail.com>
2023-08-12 10:54:35 -0500

    Updated Grudge Animation
    Co-Authored-By: SoupPotato <22991349+SoupPotato@users.noreply.github.com>

--- constants/battle_anim_constants.asm
+++ constants/battle_anim_constants.asm
@@ -272,6 +272,7 @@ DEF BATTLEANIM_BASE_TILE EQU 7 * 7  ; Maximum size of a pokemon picture
 	const ANIM_OBJ_IMPRISON_RING
 	const ANIM_OBJ_RED_X
 	const ANIM_OBJ_DESCENDING_SPARKLE
+	const ANIM_OBJ_GRUDGE
 DEF NUM_ANIM_OBJS EQU const_value
 
 ; DoBattleAnimFrame arguments (see engine/battle_anims/functions.asm)
--- data/moves/animations_2.asm
+++ data/moves/animations_2.asm
@@ -1072,12 +1072,26 @@ BattleAnim_Refresh:
 	anim_jump BattleAnim_Glimmer_branch
 
 BattleAnim_Grudge:
-	anim_1gfx ANIM_GFX_HIT
-	anim_sound 0, 1, SFX_POUND
-	anim_obj ANIM_OBJ_PALM, 136, 56, $0
-	anim_wait 6
-	anim_obj ANIM_OBJ_HIT_YFIX, 136, 56, $0
+	anim_setobjpal PAL_BATTLE_OB_RED, PAL_BTLCUSTOM_WILL_O_WISP
+	anim_1gfx ANIM_GFX_FIRE
+	anim_bgp $1b
+	anim_sound 0, 1, SFX_SPITE
+	anim_obj ANIM_OBJ_GRUDGE, 48, 80, $0
 	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 96, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 88, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 92, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 98, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 80, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 92, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_GRUDGE, 48, 88, $0
+	anim_wait 64
 	anim_ret
 
 BattleAnim_Snatch:
