6b67dd475240252460f0a2b35e88020bb98698a8
8bitZeta <devin94@protonmail.com>
2023-08-14 02:27:45 -0500

    Split Later Animations to Animations 3
    This is because Animations 2 is going to grow too large soon

--- data/moves/animation_pointers.asm
+++ data/moves/animation_pointers.asm
@@ -1,7 +1,8 @@
 BattleAnimations::
 	indirect_table 2, 0
 	indirect_entries BEAT_UP, MoveAnimations1
-	indirect_entries NUM_ATTACKS, MoveAnimations2
+	indirect_entries DEFOG, MoveAnimations2
+	indirect_entries NUM_ATTACKS, MoveAnimations3
 	indirect_entries SPECIAL_ANIM_OFFSET - NUM_SPECIAL_ANIMS - 1
 	indirect_entries SPECIAL_ANIM_OFFSET - 1, SpecialAnimations
 	indirect_table_end
@@ -9,4 +10,5 @@ BattleAnimations::
 
 INCLUDE "data/moves/animations_1.asm"
 INCLUDE "data/moves/animations_2.asm"
+INCLUDE "data/moves/animations_3.asm"
 INCLUDE "data/moves/special_animations.asm"
