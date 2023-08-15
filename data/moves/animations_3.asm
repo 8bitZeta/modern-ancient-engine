65c11bf1e09f304f0018fc03134bce9bf126c00a
8bitZeta <devin94@protonmail.com>
2023-08-15 12:24:15 -0500

    Updated Captivate Animation
    Co-Authored-By: SoupPotato <22991349+SoupPotato@users.noreply.github.com>

--- data/moves/animations_3.asm
+++ data/moves/animations_3.asm
@@ -645,6 +645,33 @@ BattleAnim_StoneEdge:
 	anim_ret
 
 BattleAnim_Captivate:
+	anim_2gfx ANIM_GFX_OBJECTS_1, ANIM_GFX_SPEED
+	anim_setobjpal PAL_BATTLE_OB_RED, PAL_BTLCUSTOM_PSYCHO_BOOST_2
+	anim_setobjpal PAL_BATTLE_OB_YELLOW, PAL_BTLCUSTOM_PSYCHO_BOOST_1
+	anim_call BattleAnim_TargetObj_1Row_3
+	anim_bgeffect ANIM_BG_FADE_MON_TO_LIGHT_REPEATING, $0, $1, $40
+	anim_bgeffect ANIM_BG_WOBBLE_MON, $0, $1, $0
+	anim_sound 0, 0, SFX_ATTRACT
+	anim_obj ANIM_OBJ_HEART_CAPTIVATE, 64, 96, $0
+	anim_wait 16
+	anim_obj ANIM_OBJ_HEART_CAPTIVATE, 32, 96, $0
+	anim_wait 16
+	anim_incbgeffect ANIM_BG_WOBBLE_MON
+	anim_call BattleAnim_ShowMon_0_3
+	anim_wait 1
+	anim_clearobjs
+	anim_sound 0, 0, SFX_MORNING_SUN
+	anim_obj ANIM_OBJ_PULSING_SPARKLE_YFIX, 32, 72, $0
+	anim_wait 5
+	anim_obj ANIM_OBJ_PULSING_SPARKLE_YFIX, 64, 104, $0
+	anim_wait 5
+	anim_obj ANIM_OBJ_PULSING_SPARKLE_YFIX, 32, 104, $0
+	anim_wait 5
+	anim_obj ANIM_OBJ_PULSING_SPARKLE_YFIX, 64, 72, $0
+	anim_wait 5
+	anim_obj ANIM_OBJ_PULSING_SPARKLE_YFIX, 48, 88, $0
+	anim_wait 12
+	anim_ret
 
 BattleAnim_StealthRock:
 
